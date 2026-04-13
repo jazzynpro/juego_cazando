7e277e9ff60adb0f78a40da71cd7f9bf3e36c10d
jazzynpro <reader_jaz@hotmail.com>
2026-04-12 22:28:05 -0500

    Parte 2 posicion gato y comida

--- cazando.js
+++ cazando.js
@@ -1,12 +1,36 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
+// Gato
+let gatoX = 0;
+let gatoY = 0;
+const ANCHO_GATO = 50;
+const ALTO_GATO = 50;
+ 
+// Comida
+let comidaX = 470;
+let comidaY = 470;
+const ANCHO_COMIDA = 30;
+const ALTO_COMIDA = 30;
 
+
+function graficarRectangulo(x, y, ancho, alto, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, ancho, alto);
+};
+ 
 function graficarGato() {
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(canvas.width/2,canvas.height/2, 50, 50);
+    graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "#000000");
+};
+ 
+function graficarComida() {
+    graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "#ff0000");
 };
  
 function iniciarJuego() {
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+    gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
     graficarGato();
+    graficarComida();
 }
+
  
